00e9fc82d90ebfa932a00c94ad85ce9eccc933f2
lewatt23 <lewatt23@gmail.com>
2022-06-17 01:37:29 +0100

    lesson 4 : pointers, passby value and pass by refrence

--- test.cpp
+++ test.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
-#include "sum.h"
-using namespace ops;
 using namespace std;
+
+// pass by reference
+int sumReference(int &a)
+{
+    a = 3;
+    return a + 3;
+}
+
+// pass by value
+int sum5(int a)
+{
+    a = 3;
+    return a + 4;
+}
 
 int main()
 {
-    cout << ops::sum(3, 2);
+    int age = 25;
+
+    cout << "age" << &age << endl;
+    cout << "sum" << sum5(age) << endl;
+
     return 0;
 }
