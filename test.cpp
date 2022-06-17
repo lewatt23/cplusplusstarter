5404d588e7572f2b8a4741696755687862c8413d
lewatt23 <lewatt23@gmail.com>
2022-06-17 21:54:28 +0100

    lesson 5: strings and strings methods

--- test.cpp
+++ test.cpp
@@ -1,26 +1,12 @@
 #include <iostream>
+#include <string>
 using namespace std;
-
-// pass by reference
-int sumReference(int &a)
-{
-    a = 3;
-    return a + 3;
-}
-
-// pass by value
-int sum5(int a)
-{
-    a = 3;
-    return a + 4;
-}
 
 int main()
 {
-    int age = 25;
+    string age = "23";
 
-    cout << "age" << &age << endl;
-    cout << "sum" << sum5(age) << endl;
+    cout << "age" << age.length() << endl;
 
     return 0;
 }
